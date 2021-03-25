b7d12ee68d4437e0b6264998c2b6fcfca4970c38
unknown <victor.curtarelli@gmail.com>
2021-03-24 21:51:51 -0300

    aula 2

---
+++ py3_basico_avancado/02/11/aula11.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print('Linha 1')
+
+'''
+print('Linha 2')
+'''
+
+# comentário 1
+# comentário 2
+print('Linha 3')  # comentário 3
+print('Linha 4')
+print('Linha 5')
+# print('Linha 6')
+# Final
---
+++ py3_basico_avancado/02/13/aula13.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 16 23:30:11 2021
+
+@author: Optimus
+"""
+
